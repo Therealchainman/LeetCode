8a684399bb7d23a8864bfed7efe3c55516ccc481
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2020-06-09 00:26:05 +0000

    Sync LeetCode submission - Power of Two (python3)

--- problems/power_of_two/solution.py
+++ problems/power_of_two/solution.py
@@ -1,5 +1,29 @@
 class Solution:
-    def isPowerOfTwo(self, n: int) -> bool:
-        if n == 0:
+    def isPowerOfTwo(self, n: int) -> bool: 
+        if n <= 0: 
             return False
-        return True if n & (n - 1) == 0 else False
+        return n & -n == n
+        
+        
+"""
+Start time: 3:43PM
+Approach1: Bit manipulation, O(1), O(1)
+&: logical operator --> check if bit is on or not.  
+16 -> 0001 0000
+&
+16 - 1 -> 0000 1111
+0
+
+1 -> 0000 0001
+&
+0 -> 0000 0000
+0
+
+7 -> 0000 0111
+&
+7 - 1 -> 0000 0110
+0000 0110
+
+
+
+"""

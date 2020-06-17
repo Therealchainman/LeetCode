99bcab21afa394d81022efb7cab0ab493c2ae398
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2020-06-17 07:12:08 +0000

    Sync LeetCode submission - Running Sum of 1d Array (python3)

--- problems/running_sum_of_1d_array/solution.py
+++ problems/running_sum_of_1d_array/solution.py
@@ -1,5 +1,12 @@
 from itertools import accumulate
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        res = accumulate(nums)
-        return list(res)
+        return list(accumulate(nums))
+    
+"""
+[1,2,3,4]
+1 + 2
+3 + 3
+6+ 4
+[1,3,6,10]
+"""

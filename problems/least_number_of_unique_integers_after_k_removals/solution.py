dbeda37d1b87647d7c120d07e4139b1f0e5ed84a
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2020-06-17 07:25:11 +0000

    Sync LeetCode submission - Least Number of Unique Integers after K Removals (python3)

--- problems/least_number_of_unique_integers_after_k_removals/solution.py
+++ problems/least_number_of_unique_integers_after_k_removals/solution.py
@@ -1,17 +1,27 @@
 from collections import Counter
 class Solution:
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
-        s = Counter(arr)
+        count = Counter(arr)
+        arrSet = set(arr)
         heap = []
-        se = set(arr)
-        for a in se:
-            heap.append((a, s[a]))
+        for a in arrSet:
+            heap.append([a, count[a]])
         heap.sort(key=lambda x: x[1], reverse=True)
         for _ in range(k):
-            elem, priority = heap[-1]
-            priority -= 1
-            heap.pop()
-            if priority > 0:
-                heap.append((elem, priority))
+            heap[-1][1] -= 1
+            if heap[-1][1] == 0:
+                heap.pop()
         return len(heap)
         
+        
+"""
+Another thing look at this example:
+[4,3,1,1,3,3,2]
+So I remove 3 elements, what is the least number of unique integers.  Well remove the 4 and 2 and you can get it to 2.  
+
+1) Sort the heap according to the priority, set reverse=True so it is in descending order, so that I reduce prirority of numbers that 
+appear the fewest number of times.  I want to remove those first after all.  
+2) get the elem and priority of the last element in heap
+3) decrement the priority, if the priority==0, then pop it from the heap. 
+
+"""

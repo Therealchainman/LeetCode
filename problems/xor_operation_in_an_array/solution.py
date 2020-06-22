d52ad0e2907e948224b92e53a22611899470a0e3
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2020-06-22 22:51:24 +0000

    Sync LeetCode submission - XOR Operation in an Array (python3)

--- problems/xor_operation_in_an_array/solution.py
+++ problems/xor_operation_in_an_array/solution.py
@@ -2,7 +2,15 @@ from itertools import accumulate
 import operator
 class Solution:
     def xorOperation(self, n: int, start: int) -> int:
-        arr = []
+        x = 0
         for i in range(n):
-            arr.append(start + 2*i)
-        return list(accumulate(arr, operator.xor))[-1]
+            x ^= (start + i*2)
+        return x
+    
+    
+"""
+[1,2,3,4,5]
+-> [1, 2+1=3, 3+3=6, 6+4=10, 1+2+3+4+5]
+[]
+accumulate(arr, operator.mul)
+"""

430459bb101512b8b5edf491da3b7046daccd226
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2020-07-14 06:42:11 +0000

    Sync LeetCode submission - Kth Largest Element in an Array (python3)

--- problems/kth_largest_element_in_an_array/solution.py
+++ problems/kth_largest_element_in_an_array/solution.py
@@ -1,25 +1,31 @@
+from random import randint
+from random import shuffle
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        return sorted(nums, reverse=True)[k-1]
+        shuffle(nums)
+        def quickSelect(arr, k, left, right):
+            if left == right:
+                return arr[left]
+            pivot = partition(arr, left, right)
+            if k - 1 == pivot:
+                return arr[pivot]
+            elif k - 1 < pivot:
+                return quickSelect(arr, k, left, pivot - 1)
+            else:
+                return quickSelect(arr, k, pivot + 1, right)
         
-            
+        def partition(arr, left, right):
+            i = left
+            for j in range(left, right):
+                if arr[j] > arr[right]:
+                    arr[i], arr[j] = arr[j], arr[i]
+                    i += 1
+            if arr[i] < arr[right]:
+                arr[i], arr[right] = arr[right], arr[i]
+            return i
+        
+        return quickSelect(nums, k, 0, len(nums) - 1)
         
 """
-Start time: 7:01PM
-approach1: nlogn
-sort array in descending
-move k iterations to the index of the answer. 
-
-approach2: O(n)
-iterate through add all elements to the priority queue
-pop from the priority queue k times
-
-O(nlogn+logk)
-
-logn+logn+logn 
---n times-----
-
-[6,5,5,4,]
-
-
+This solution is using quick select algorithm
 """

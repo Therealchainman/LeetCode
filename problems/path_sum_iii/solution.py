c2fde7e132e38d827eb3b93b9b559609724da23f
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2020-08-10 04:15:35 +0000

    Sync LeetCode submission - Path Sum III (python3)

--- problems/path_sum_iii/solution.py
+++ problems/path_sum_iii/solution.py
@@ -6,29 +6,22 @@
 #         self.right = right
 class Solution(object):
     def pathSum(self, root, target):
-        # define global result and path
-        self.result = 0
-        cache = {0:1}
-        
-        # recursive to get result
-        self.dfs(root, target, 0, cache)
-        
-        # return result
-        return self.result
+        if not root:
+            return 0
+        self.P={0:1}
+        self.ans=0
+        self.dfs(root,target,root.val)
+        return self.ans
     
-    def dfs(self, root, target, currPathSum, cache):
-        # exit condition
-        if root is None:
-            return  
-        # calculate currPathSum and required oldPathSum
-        currPathSum += root.val
-        oldPathSum = currPathSum - target
-        # update result and cache
-        self.result += cache.get(oldPathSum, 0)
-        cache[currPathSum] = cache.get(currPathSum, 0) + 1
+    def dfs(self, root, t, val):
+        self.ans += self.P.get(val-t,0)
+        self.P[val] = self.P.get(val,0) + 1
         
-        # dfs breakdown
-        self.dfs(root.left, target, currPathSum, cache)
-        self.dfs(root.right, target, currPathSum, cache)
-        # when move to a different branch, the currPathSum is no longer available, hence remove one. 
-        cache[currPathSum] -= 1
+        if root.left:
+            self.dfs(root.left,t,val+root.left.val)
+        if root.right:
+            self.dfs(root.right,t,val+root.right.val)
+        # remove the fact that this value appeared this many times because
+        # we are moving to another branch. 
+        self.P[val]-=1
+        

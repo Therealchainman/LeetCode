a8e4a95f430f2eaec8ae8e4eda29569edad16c6e
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2020-08-12 05:39:30 +0000

    Sync LeetCode submission - Minimum Cost to Cut a Stick (python3)

--- problems/minimum_cost_to_cut_a_stick/solution.py
+++ problems/minimum_cost_to_cut_a_stick/solution.py
@@ -1,23 +1,22 @@
+from functools import lru_cache
 class Solution:
-    def minCost(self, n, cuts):
-        A=[0]+sorted(cuts)+[n]
-        dp=[[0]*len(A) for _ in A]
-        
+    def minCost(self, n, A):
+        A = [0] + sorted(A) + [n]
+        dp=[[0]*len(A) for _ in range(len(A))]
         for i in range(len(A)-2,-1,-1):
             for j in range(i+2,len(A)):
                 dp[i][j]=float('inf')
                 for k in range(i+1,j):
-                    cand=A[j]-A[i]+dp[i][k]+dp[k][j]
-                    dp[i][j]=min(dp[i][j],cand)
+                    dp[i][j]=min(dp[i][j],A[j]-A[i]+dp[i][k]+dp[k][j])
         return dp[0][-1]
-        
-        # A=[0]+sorted(cuts)+[n]
         # @lru_cache(None)
         # def dp(i,j):
+        #     print(i,j)
+        #     print(A[j],A[i])
         #     if i+1>=j:
         #         return 0
         #     ans=float('inf')
         #     for k in range(i+1,j):
-        #         ans=min(ans,A[j]-A[i]+dp(i,k)+dp(k,j))
+        #         ans=min(ans, A[j]-A[i]+dp(i,k)+dp(k,j))
         #     return ans
         # return dp(0,len(A)-1)

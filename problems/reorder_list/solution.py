281328bbc4d7de1c2f1ecb27c259ac146c80f802
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2020-08-21 23:06:16 +0000

    Sync LeetCode submission - Reorder List (python3)

--- problems/reorder_list/solution.py
+++ problems/reorder_list/solution.py
@@ -1,60 +1,29 @@
-# Splits in place a list in two halves, the first half is >= in size than the second.
-# @return A tuple containing the heads of the two halves
-def _splitList(head):
-    fast = head
-    slow = head
-    while fast and fast.next:
-        slow = slow.next
-        fast = fast.next
-        fast = fast.next
-
-    middle = slow.next
-    slow.next = None
-
-    return head, middle
-
-# Reverses in place a list.
-# @return Returns the head of the new reversed list
-def _reverseList(head):
-
-  last = None
-  currentNode = head
-
-  while currentNode:
-    nextNode = currentNode.next
-    currentNode.next = last
-    last = currentNode
-    currentNode = nextNode
-
-  return last
-
-# Merges in place two lists
-# @return The newly merged list.
-def _mergeLists(a, b):
-
-    tail = a
-    head = a
-
-    a = a.next
-    while b:
-        tail.next = b
-        tail = tail.next
-        b = b.next
-        if a:
-            a, b = b, a
-            
-    return head
-
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-
-    # @param head, a ListNode
-    # @return nothing
     def reorderList(self, head):
-
-        if not head or not head.next:
-            return
-
-        a, b = _splitList(head)
-        b = _reverseList(b)
-        head = _mergeLists(a, b)
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head:
+            return head
+        slow=head
+        fast=slow.next
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        cur = slow.next
+        stk=[]
+        while cur:
+            stk.append(cur)
+            cur=cur.next
+        cur=head
+        while stk:
+            stk[-1].next=cur.next
+            cur.next=stk.pop()
+            cur=cur.next.next
+        cur.next=None
+            

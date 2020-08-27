e92f3e0498a0f725f5529be71db6994e4962f94b
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2020-08-27 19:32:30 +0000

    Sync LeetCode submission - Minimum Cost For Tickets (python3)

--- problems/minimum_cost_for_tickets/solution.py
+++ problems/minimum_cost_for_tickets/solution.py
@@ -1,47 +1,47 @@
-from collections import deque
+
+import bisect
 class Solution:
-    def mincostTickets(self, D,c):
-        k=3
-        P=[1,7,30]
-        Q=[deque() for _ in range(k)]
-        cost=0
-        for d in D:
-            for i in range(k):
-                while Q[i] and Q[i][0][0]+P[i]<=d:
-                    Q[i].popleft()
-                Q[i].append((d,cost+c[i]))
-            cost = min(Q[i][0][1] for i in range(k))
-        return cost
+    def mincostTickets(self, days,costs):
+        
+        @lru_cache(None)
+        def dp(i):
+            if i>=len(days):
+                return 0
+            return min(costs[0]+dp(bisect.bisect_left(days,days[i]+1)),
+                      costs[1]+dp(bisect.bisect_left(days,days[i]+7)),
+                      costs[2]+dp(bisect.bisect_left(days,days[i]+30))
+                      )
+        return dp(0)
         
 """
-start time: 1:26AM
-approach1:  DP
-My first solution didn't work so redoing this with a queue now, 
-Suppose we have the following days, 1,2,3,4,5,6,7,8,9,10,30,31
-So then we take 
-we update queue for 1, 7, and 30 days
-[1,2]   [1,7]  [1,15]
-next day we get
-[2,4]   [[1,7],[2,9]]  [[1,15],[2,17]]
-think about what this is doing this is basically adding 7 days to day 2 or starting the travel there which means we 
-are at a cost of 9 because that is our minimal cost so far, 
-This will repeat until
-[7,14] [[1,7],[2,9],[3,11],[4,13],[5,9],[6,11],[7,13]]
-then we realize that we can pop day 7 because the 1+7>d, 8>7 so that means we are over 7 days
-so then we look at miminum cost,
-[4,8], so now it choose 7 as cheaper
-and now we get to 
+What is this lol?  So I have in my notes that this is a binary search problem.  but let's think about this for a moment.  The questions is asking to output the lowest cost for buying tickets right,  so I need to say there is a 1-day pass, a 7-day pass and another 1 day pass righgt,  sure but how do I find that 
 
-[1,2]   [1,7],  [1,15]
-[2,4]   [[1,7],[2,9]]   [[1,15],[1,17]]
-[3,6]   [[1,7],[2,9],[3,11]]  [[1,15],[2,17],[3,19]]
-cost=9
-day 9
-[9,11]  [[2,9],[3,11],[4,13],[5,14],[6,14],[7,14],[8,14]]  [[1,15],[2,17],[3,19],[4,21],[5,22],[6,22],[7,22],[8,22]]
-1+7=8
-1-7, 7 
-6+7=13
+I guess you could iterate through days, maybe, 
+
+let think of the three tickets that I can get and how they relate to binary search.  
+
+So for starters I could get ticket 1
+so lets do a binary search to find the index I would reach in the table I guess,  
 
 
+[1,4,6,7,8,20]
+so search for 1+1=2, 
+returns TFFFFF, lets return the first occurrence of true right and that would be the case for the algo that we move lo
+then we take 7 day ticket and do 
+1+7=8, and we iterate through and we will have TTTTFF, so we will say it is true only if less than the current value in iteration. 
+then we have a simple problem becaues it is being broken down
+Then we have that indexvalue and we continue our search from that index value.  
+Another is the 30 days, 
+For each of these we want to return the minimum total cost,  so it is just plus the rest, so we take the current cost and add
+and this continues until base case where, if i is at end of list we return 0 because we do not need to travel anymore you know what I mean lol, this is just horrendous, so the base case would be if we run out of days right, then return 0
+and at the end we want to return the minimum, also we can use dp because we will see similar cases, but ultimately if say we are at i=4,  if we see i=4 again it is the same thing, that is this problem can be broken down into smaller pieces 
 
+Let's analyze what is happening here real quick.  So we have 
+[1,4,6,7,8,20]
+ans we have TFFFFFF, oh that is why it returns T, fair,  ummm,  I could switch it to 
+FTTTTT, in which case anything that is greater than the target is true.  then I want to return the first occurrence of true. 
+
+
+FFFFFF
 """
+        

6f48783859ea7d9f806c58c1b670f6565060fd6b
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2020-08-28 15:25:42 +0000

    Sync LeetCode submission - Implement Rand10() Using Rand7() (python3)

---
+++ problems/implement_rand10()_using_rand7()/solution.py
@@ -0,0 +1,17 @@
+# The rand7() API is already defined for you.
+# def rand7():
+# @return a random integer in the range 1 to 7
+
+class Solution:
+    def rand10(self):
+        rand40=40
+        while rand40>=40:
+            rand40=7*(rand7()-1)+(rand7()-1)
+        return rand40 % 10 + 1
+        
+"""
+Good question, haha, I don't know how to solve this lol.
+This seems like probablity or something, I mean it says rand7, but I don't know what that means haha
+
+"""
+        

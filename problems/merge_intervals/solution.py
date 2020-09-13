82cc0ad2807bb7e90f77b4268ee34850e1af851c
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2020-09-13 19:45:22 +0000

    Sync LeetCode submission - Merge Intervals (python3)

--- problems/merge_intervals/solution.py
+++ problems/merge_intervals/solution.py
@@ -1,15 +1,18 @@
 class Solution:
     def merge(self, A):
-        n=len(A)
-        if n<=1:
-            return A
-        A.sort()
-        ret=[A[0]]
-        for i in range(1,n):
-            s,e=A[i]
-            if ret[-1][1]>=s:
-                ret[-1][1]=max(ret[-1][1],e)
-            else:
-                ret.append(A[i])
-        return ret
+        events=[]
+        for s,e in A:
+            events.append((s,1))
+            events.append((e,-1))
+        count=0
+        B=[]
+        events.sort(key=lambda x: [x[0],-x[1]])
+        for event, inc in events:
+            count+=inc
+            if count==1 and inc==1:
+                start=event
+            if count==0 and inc==-1:
+                B.append([start,event])
+                start=None
+        return B
         

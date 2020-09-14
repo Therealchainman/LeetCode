73f3be77829360f6afbd47f1e1e4d02fb5f536bc
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2020-09-14 03:24:40 +0000

    Sync LeetCode submission - Task Scheduler (python3)

--- problems/task_scheduler/solution.py
+++ problems/task_scheduler/solution.py
@@ -1,6 +1,23 @@
 class Solution:
-    def leastInterval(self, tasks: List[str], n: int) -> int:
-        freq = Counter(tasks)
-        Most_freq = freq.most_common()[0][1]
-        Found_most = sum([freq[key] == Most_freq for key in freq])
-        return max(len(tasks), (Most_freq - 1) * (n + 1) + Found_most)
+    def leastInterval(self, T,n):
+        count=[0]*26
+        offset=ord("A")
+        for t in T:
+            count[ord(t)-offset]+=1
+        count.sort()
+        max_val=count[25]-1
+        idle_slots=max_val*n
+        for i in range(24,-1,-1):
+            idle_slots-=min(count[i],max_val)
+        return idle_slots+len(T) if idle_slots>=0 else len(T)
+
+            
+                     
+        
+        
+"""
+A->B->idle->A->B->idle->A->B
+t=1,2,3
+heap=[(A,3),(B,4)]
+"""
+        

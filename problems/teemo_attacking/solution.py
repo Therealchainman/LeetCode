c0a41f6e3ebbf0b178db3378f5791f3dc22018ca
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2020-09-26 16:13:00 +0000

    Sync LeetCode submission - Teemo Attacking (python3)

---
+++ problems/teemo_attacking/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findPoisonedDuration(self, timeSeries, duration):
+        n, out = len(timeSeries), 0
+        if n == 0: return 0
+        for i in range(n-1):
+            out += min(timeSeries[i+1] - timeSeries[i], duration)
+        return out + duration
+        
+            
+        

549b4fc5b7dbe53f188220cc45038c75c80c3363
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2020-10-16 07:47:37 +0000

    Sync LeetCode submission - Search a 2D Matrix (python3)

--- problems/search_a_2d_matrix/solution.py
+++ problems/search_a_2d_matrix/solution.py
@@ -1,51 +1,33 @@
 class Solution:
-    def searchMatrix(self, M,target):
-        if not M:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
             return False
-        if not M[0]:
+        if not matrix[0]:
             return False
-        
-        R,C=len(M),len(M[0])
-        
-        def row_by_row(r,target):
-            return M[r][0]<=target
-        
-        def target_in_row(r,c,target):
-            return M[r][c]>=target
+        R,C=len(matrix),len(matrix[0])
+        def possible(val,target):
+            return val<=target
         
         def binary_search_row():
             lo,hi=0,R-1
-            while lo < hi:
+            while lo<hi:
                 mid=lo+hi+1>>1
-                if row_by_row(mid,target):
+                if possible(matrix[mid][0],target):
                     lo=mid
                 else:
                     hi=mid-1
             return lo
         
-        def binary_search_in_row(row):
+        def binary_search_col(row):
             lo,hi=0,C-1
-            while lo < hi:
-                mid=lo+hi>>1
-                if not target_in_row(row,mid,target):
-                    lo=mid+1
+            while lo<hi:
+                mid=lo+hi+1>>1
+                if possible(matrix[row][mid],target):
+                    lo=mid
                 else:
-                    hi=mid
-            return M[row][lo]
+                    hi=mid-1
+            return lo
         
-        row=binary_search_row()
-        cand_val=binary_search_in_row(row)
-        return target==cand_val
-                    
-            
-        
-"""
-binary search row by row
-TFF return last the occurrence of T. 
-
-found the row tha tyo ushould search
-binary search a row
-FTTT return the first occurrence of T. 
-TFFF
-"""
-        
+        x=binary_search_row()
+        y=binary_search_col(x)
+        return matrix[x][y]==target

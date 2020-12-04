ac809039201155aab12848979e58402594d29be0
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2020-12-04 09:44:17 +0000

    Sync LeetCode submission - The kth Factor of n (golang)

---
+++ problems/the_kth_factor_of_n/solution.go
@@ -0,0 +1,21 @@
+func kthFactor(n int, k int) int {
+    root := math.Sqrt(float64(n))
+    for i:=1;i<int(math.Ceil(root));i++ {
+        if n%i==0 {
+            k--
+        }
+        if k==0 {
+            return i
+        }
+    }
+    for i:=int(math.Floor(root));i>0;i-- {
+        if n%i==0 {
+            k--
+        }
+        if k==0 {
+            return n/i
+        }
+    }
+    return -1
+    
+}

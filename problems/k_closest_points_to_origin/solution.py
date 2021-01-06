c28df21d17031d79731c2bcbb9d5f6b8161f13dd
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2021-01-06 00:29:07 +0000

    Sync LeetCode submission - K Closest Points to Origin (python3)

--- problems/k_closest_points_to_origin/solution.py
+++ problems/k_closest_points_to_origin/solution.py
@@ -1,15 +1,36 @@
-import heapq
-
+import random
 class Solution:
-    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+    def kClosest(self,points,K):
+        ## Approach : Random quick sort partitioning (O(n))
+        if K == len(points) :
+            return points
+        dist = lambda p: p[0]**2 + p[1]**2
+        compare = lambda p1,p2 : dist(p1) - dist(p2)
+
+        def partition(start, end) :
+            p = random.randint(start,end) 
+            points[p] ,points[start] = points[start],points[p] # Swap pivot with right most ele
+            l, r  = start , end
+            while True:
+                while l < r and  compare(points[r], points[start]) >= 0 :
+                    r -= 1
+                while l < r and compare(points[l] , points[start]) <= 0:
+                    l += 1
+                if  l<r :
+                    points[l] , points[r] = points[r], points[l]
+                else :
+                    break
+            points[start], points[r] = points[r] , points[start]
+            return r
+
+        i, j = 0 , len(points) -1
+        while i <= j:
+            pivot = partition(i,j)
+            if pivot  ==  K-1 :
+                return points[:K]
+            elif pivot < K -1 :
+                i = pivot + 1
+            else :
+                j = pivot - 1
         
-        heap = []
         
-        for (x, y) in points:
-            dist = -(x*x + y*y)
-            if len(heap) == K:
-                heapq.heappushpop(heap, (dist, x, y))
-            else:
-                heapq.heappush(heap, (dist, x, y))
-        
-        return [(x,y) for (dist,x, y) in heap]

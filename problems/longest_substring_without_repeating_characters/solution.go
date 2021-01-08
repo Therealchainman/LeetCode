7ce3cce32e88cabd31a96277438aca1b4b8cf707
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2021-01-08 05:42:35 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (golang)

--- problems/longest_substring_without_repeating_characters/solution.go
+++ problems/longest_substring_without_repeating_characters/solution.go
@@ -1,45 +1,22 @@
-func lengthOfLongestSubstring(s string) int {
-    count_chars:=make(map[byte]int)
-    counter:=0
-    left:=0
-    right:=0
-    best:=0
-    // Iterate until end of string and increment counter when the character is already in the hashmap.
-    for right<len(s) {
-        ch:=s[right]
-        if cnt,found:=count_chars[ch]; found {
-            if cnt==1 {
-                counter+=1
-            }
-            count_chars[ch]=count_chars[ch]+1
-        } else {
-            count_chars[ch]=1
-        }
-        // The counter is the number of numbers that are repeated in the string.  We want to move the left pointer
-        // until there are 0 repeated characters in the string.  
-        for counter > 0 {
-            if cnt,found:=count_chars[s[left]]; found {
-                if cnt==2 {
-                    counter-=1
-                    count_chars[s[left]]=count_chars[s[left]]-1
-                } else if cnt==1 {
-                    delete(count_chars,s[left])
-                } else {
-                    count_chars[s[left]]=count_chars[s[left]]-1
-                }
-                
-            }
-            left+=1
-        }
-        best=Max(best,right-left+1)
-        right+=1
+func max(x, y int) int {
+    if x > y {
+        return x
     }
-    return best
+    return y
 }
-
-func Max(x,y int) int {
-    if x < y {
-        return y
+func lengthOfLongestSubstring(s string) int {
+    count := make(map[byte]int)
+    ans := 0
+    le := 0
+    ri := 0
+    for ri<len(s) {
+        count[s[ri]]++
+        for count[s[ri]]>1 {
+            count[s[le]]--
+            le++
+        }
+        ans = max(ans, ri - le + 1)
+        ri++
     }
-    return x
+    return ans
 }

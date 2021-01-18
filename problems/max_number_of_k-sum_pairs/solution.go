4a0f204e530ac3109e4eb2b863560e622bbd2388
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2021-01-18 23:51:20 +0000

    Sync LeetCode submission - Max Number of K-Sum Pairs (golang)

--- problems/max_number_of_k-sum_pairs/solution.go
+++ problems/max_number_of_k-sum_pairs/solution.go
@@ -1,19 +1,24 @@
+func min(x, y int) int {
+    if x < y {
+        return x
+    }
+    return y
+}
+
 func maxOperations(nums []int, k int) int {
-    sort.Ints(nums)
-    n := len(nums)
-    lo := 0
-    hi := n - 1
+    freq := make(map[int]int)
+    for _, x := range nums {
+        freq[x]++
+    }
     ans := 0
-    for lo < hi {
-        total := nums[lo]+nums[hi]
-        if total == k {
-            ans++
-            hi--
-            lo++
-        } else if total < k {
-            lo++
-        } else {
-            hi--
+    for val, cnt := range freq {
+        if val + val == k {
+            ans += cnt/2
+        } else if freq[k-val] > 0 {
+            delta := min(cnt, freq[k-val])
+            ans+=delta
+            freq[k-val]-=delta
+            freq[val]-=delta
         }
     }
     return ans

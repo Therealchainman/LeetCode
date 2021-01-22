18cd780268e9f1c5d9d5ff960737a670f970c6e5
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2021-01-22 01:54:01 +0000

    Sync LeetCode submission - Find the Most Competitive Subsequence (golang)

--- problems/find_the_most_competitive_subsequence/solution.go
+++ problems/find_the_most_competitive_subsequence/solution.go
@@ -1,18 +1,14 @@
 func mostCompetitive(nums []int, k int) []int {
-    ans := []int{}
-    numRemove := len(nums) - k
-    for _,x := range nums {
-        for len(ans)>0 && x<ans[len(ans)-1] && numRemove>0 {
-            ans[len(ans)-1]=0
+    var ans []int
+    var p int
+    for i, x := range nums {
+        p = len(nums)-i
+        for len(ans)>0 && x < ans[len(ans)-1] && p - (k - len(ans)) > 0 {
             ans = ans[:len(ans)-1]
-            numRemove--
         }
-        ans = append(ans, x)
-    }
-    for numRemove>0 {
-        ans[len(ans)-1]=0
-        ans = ans [:len(ans)-1]
-        numRemove--
+        if len(ans) < k {
+            ans = append(ans, x)
+        }
     }
     return ans
 }

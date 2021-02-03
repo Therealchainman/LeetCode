00226b488cea38bbfd89555734a065e3bfb50ed5
Todd Chaney <47466952+Therealchainman@users.noreply.github.com>
2021-02-03 22:38:24 +0000

    Sync LeetCode submission - Linked List Cycle (golang)

--- problems/linked_list_cycle/solution.go
+++ problems/linked_list_cycle/solution.go
@@ -6,15 +6,12 @@
  * }
  */
 func hasCycle(head *ListNode) bool {
-    if head == nil {
-        return false
-    }
-    var slow *ListNode = head
-    var fast *ListNode = head
-    for fast.Next != nil && fast.Next.Next != nil {
-        slow=slow.Next
-        fast=fast.Next.Next
-        if (fast==slow) {
+    slow := head
+    fast := head
+    for slow != nil && fast.Next != nil && fast.Next.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+        if fast == slow {
             return true
         }
     }
